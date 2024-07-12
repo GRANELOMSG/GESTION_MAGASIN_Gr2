9d52d00ca3d6f8c9f20d1212cbdf85f6ec958d91
MONGA SEYA Granel <22ms263@esisalama.org>
2024-07-12 20:11:32 +0100

    partie 2

--- supprimer_produit.py
+++
@@ -1,9 +0,0 @@
-from charger_donnees import*
-from stocker_donnees import*
-from Recherche import*
-
-def supprimer_produits():
-    liste_produits=charger_produit()
-    verificateur = charger_produit()
-    if verificateur == 1: 
-        pass
